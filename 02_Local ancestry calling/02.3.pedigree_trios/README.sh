2c3f3cd38d0bd8e2fc504b9f0e7eadc9e677a8d7
ArielleF <afogel29@gmail.com>
2021-07-14 19:03:59 -0400

    Update README.sh

--- 02_Local ancestry calling/02.3.pedigree_trios/README.sh
+++ 02_Local ancestry calling/02.3.pedigree_trios/README.sh
@@ -1,18 +1,46 @@
 # This directory contains multiple scripts for evaluating the quality of local ancestry calls from LCLAE using known parent-offspring trios from the Amboseli baboons (Supplementary Methods 7.3).
 
-# In R, run 1prepare_for_permutations.R which generates two R data files (local_ancestry_pedigree_trios_maskedSNPRCref.Rd and local_ancestry_pedigree_trios_unmaskedWallref.Rd) containing tracts, pedigree trio info, list of pedigree individuals, and genomic positions for evaluating the consistent of ancestry calls within pedigree trios for two sets of ancestry tracts (one generated using the SNPRC reference panel, one generated using the Wall et al. 2016 Molecular Ecology low coverage reference panel). These R data files can then uploaded to a computing cluster for parallelization across chromosomes (which will make things run much faster).
+# In R, run 1prep_for_pedigree_inconsistencies.R which generates two R data files (local_ancestry_pedigree_trios_maskedSNPRCref.Rd and local_ancestry_pedigree_trios_unmaskedWallref.Rd) containing tracts, pedigree trio info, list of pedigree individuals, and genomic positions for evaluating the consistent of ancestry calls within pedigree trios for two sets of ancestry tracts (one generated using the SNPRC reference panel, one generated using the Wall et al. 2016 Molecular Ecology low coverage reference panel). These R data files can then uploaded to a computing cluster for parallelization across chromosomes (which will make things run much faster).
 
-# In a directory on a computing cluster containing both R data files, run the scripts 2agroom_perm.R and 2bprox_perm.R as arrays (see below) which will:
+# In a directory on a computing cluster containing both R data files, run the R scripts 2get_ancestry_calls_SNPRCref.R and 2get_ancestry_calls_Wallref.R using the commands below in order to generate chromosome-specific scripts. These scripts (which only differ in the ancestry calls they are using) will generate chromosome-specific files containing the ancestry calls ancestry calls for each individual in the pedigree analysis at each of our focal positions (positions data frame created in 1prep_for_pedigree_inconsistencies.R):
+# R must be loaded in your computing environment (e.g., module load R, activate a conda environment with R loaded, etc.)
+for f in `cat autosomes.list`; do sed -e s/CHROMOSOME/$f/g 2get_ancestry_calls_SNPRCref.R > $f.sh; sbatch --mem=300 $f.sh; rm $f.sh; done
+for f in `cat autosomes.list`; do sed -e s/CHROMOSOME/$f/g 2get_ancestry_calls_Wallref.R > $f.sh; sbatch --mem=300 $f.sh; rm $f.sh; done
+
+# Concatenate the output of the chromosome-specific files into a single file for set of ancestry calls (i.e., SNPRC or Wall et al. reference panels)
+for file in ancestry_calls*
+do
+     tail -n +2 "$file" >> "nohead.$file.txt" 
+done
+
+# Grab the header from one of the files
+head -1 local_ancestry_calls_maskedfullref_pedigree_35kbpos_chr1.txt >> header
+# Add the header to the concatenated file
+cat header nohead.local_ancestry_calls_maskedfullref_pedigree_35kbpos_chr* >> all.local_ancestry_calls_maskedfullref_pedigree_35kbpos.txt
+rm *head*
+
+# get pedigree inconsistencies
+sbatch --mem=1G run.02.ped_inconsistencies.sh
+
+# get AIM count
+for f in `seq 1 20`; do sed -e s/CHROMOSOME/$f/g 3pos_AIM_count.sh > $f.sh; sbatch --mem=30000 $f.sh; rm $f.sh; done
 
 
 
-# (a) randomly permute the grooming/proximity probabilities computed in 1prepare_for_permutations.R across all female two-month intervals, 
-# (b) randomly assign 0/1 by drawing from a binomial distribution with probability equal to the permuted grooming/proximity probability for each female two-month interval, and
-# (c) fit the same grooming/proximity model used to analyze the real data but using the new, permutation-based outcome variable.
-# Run 50 jobs per array (since each job is currently set to run 20 permutations which can be changed by setting nperm to a value other than 20 in groom_perm.R and prox_perm.R) because we want to run 1,000 permutations per model in total (50 jobs * 20 permutations/job = 1,000 permutations total)
-# module load R before running script since the groom_perm.R and prox_perm.R scripts are set up to load our current environment
-sbatch --mem=100 --cpus-per-task=20 --array=1-50%5 2agroom_perm.R
-sbatch --mem=100 --cpus-per-task=20 --array=1-50%5 2bprox_perm.R 
+# get first four columns (chrom, pos, start_window, end_window)
+awk '{print $1,$2,$3,$4}' indiv.pos_AIM_count_1kbwin_HAP.txt >> tmp
+for f in `ls indiv.pos_AIM_count_1kbwin_*`; do awk '{print $5}' $f >> tmp1.$f; echo $f; done
+for f in `ls tmp1.indiv.pos_AIM_count_1kbwin_*`; do paste $f >> tmp1.$f; echo $f; done
+
+# check Ns in chr7
+perl -ne 'chomp;if( />(.*)/){$head = $1; $i=0; next};@a=split("",$_); foreach(@a){$i++; if($_ eq "N" && $s ==0 ){$z=$i-1; print "$head\t$z"; $s =1}elsif($s==1 && $_ ne "N"){$j=$i-1;print "\t$j\n";$s=0}}' chr7.test.fa >> check_Ns
+
+# SNP count
+
+for f in `seq 1 20`; do awk -v var=$f '{OFS="\t";if ($1==var) print }' SNP_count_35kbwin.$f.txt > SNP_count_35kbwin.$f.final.txt; done
+
+
+
 
 # Concatenate the output of the 50 jobs per array (20 permutations per job) into a single file for each model
 cat groom_permuted_results_*.txt >> all.groom_permuted_results.txt
