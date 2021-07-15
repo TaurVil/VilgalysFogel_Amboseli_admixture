b6dee2cfd4ee6be7320c7f82fe9fa3e10859c2e6
ArielleF <afogel29@gmail.com>
2021-07-14 21:00:27 -0400

    Update README.sh

--- 02_Local ancestry calling/02.3.pedigree_trios/README.sh
+++ 02_Local ancestry calling/02.3.pedigree_trios/README.sh
@@ -11,73 +11,57 @@ grep "done" slurm* | wc -l #40 = the number of total scripts we ran so everythin
 
 # Concatenate the output of the chromosome-specific files into a single file for each set of ancestry calls (i.e., SNPRC or Wall et al. reference panels)
 # SNPRC
+# Remove the header of each file
 for file in ancestry_calls_maskedSNPRCref*
 do
-     tail -n +2 "$file" >> "nohead.$file.txt" 
+     tail -n +2 "$file" >> "nohead.$file" 
 done
 
-# Grab the header from one of the files
+# Grab the header from one of the original files with a header (all files should have the same header so it should not matter which chromosome file you choose)
 head -1 ancestry_calls_maskedSNPRCref_pedigree_trios_35kbpos_chr1.txt >> header
-# Add the header to the concatenated file
-cat header nohead.local_ancestry_calls_maskedfullref_pedigree_35kbpos_chr* >> all.local_ancestry_calls_maskedfullref_pedigree_35kbpos.txt
+# Concatenate the header and all of the header-less files into a single file
+cat header nohead.* >> all.ancestry_calls_maskedSNPRCref_pedigree_trios_35kbpos.txt
 rm *head*
 
 # Wall
-or file in ancestry_calls_unmaskedWallref*
+for file in ancestry_calls_unmaskedWallref*
 do
-     tail -n +2 "$file" >> "nohead.$file.txt" 
+     tail -n +2 "$file" >> "nohead.$file" 
 done
 
-# Grab the header from one of the files
+# Grab the header from one of the original files with a header (all files should have the same header so it should not matter which chromosome file you choose)
 head -1 ancestry_calls_unmaskedWallref_pedigree_trios_35kbpos_chr1.txt >> header
-# Add the header to the concatenated file
-cat header nohead.local_ancestry_calls_maskedfullref_pedigree_35kbpos_chr* >> all.local_ancestry_calls_maskedfullref_pedigree_35kbpos.txt
+# Concatenate the header and all of the header-less files into a single file
+cat header nohead.* >> all.ancestry_calls_unmaskedWallref_pedigree_trios_35kbpos.txt
 rm *head*
 
 # Delete chromsome-specific files as these results are now stored in either the all files generated above
 rm ancestry*
 
-# Check that we have the expected number of total lines per set of ancestry calls (should equal the total number of positions = 73975)
+# Check that we have the expected number of total lines per set of ancestry calls (should equal the total number of positions + header = 73975 + 1 = 73976)
 wc -l all*
+#73976 all.ancestry_calls_maskedSNPRCref_pedigree_trios_35kbpos.txt
+#73976 all.ancestry_calls_unmaskedWallref_pedigree_trios_35kbpos.txt
 
-
-# get pedigree inconsistencies
-sbatch --mem=1G 3ped_inconsistencies.sh
+# In the same directory on a computing cluster, run the R scripts
+sbatch --mem=1G  3get_ped_inconsistencies_SNPRCref.R
+sbatch --mem=1G  3get_ped_inconsistencies_Wallref.R
 
 # For each genomic window, we would also like to get information on the number of ancestry informative markers, FST, and recombination rate which we will include as covariates in our models of pedigree inconsistencies
-# Run the R script 3apos_AIM_count.sh using the command below in order to generate chromosome-specific scripts.
-for f in `seq 1 20`; do sed -e s/CHROMOSOME/$f/g 3apos_AIM_count.sh > $f.sh; sbatch --mem=30000 $f.sh; rm $f.sh; done
+# For the number of ancestry informative markers, run the R script 3apos_AIM_count.sh using the command below in order to generate chromosome-specific scripts.
+for f in `seq 1 20`; do sed -e s/CHROMOSOME/$f/g 4apos_AIM_count.sh > $f.sh; sbatch --mem=30000 $f.sh; rm $f.sh; done
 
-
-
-
-# get first four columns (chrom, pos, start_window, end_window)
+# Get first four columns (chrom, pos, start_window, end_window)
 awk '{print $1,$2,$3,$4}' indiv.pos_AIM_count_1kbwin_HAP.txt >> tmp
 for f in `ls indiv.pos_AIM_count_1kbwin_*`; do awk '{print $5}' $f >> tmp1.$f; echo $f; done
 for f in `ls tmp1.indiv.pos_AIM_count_1kbwin_*`; do paste $f >> tmp1.$f; echo $f; done
 
+# For FST, run the script
+
 # check Ns in chr7
-perl -ne 'chomp;if( />(.*)/){$head = $1; $i=0; next};@a=split("",$_); foreach(@a){$i++; if($_ eq "N" && $s ==0 ){$z=$i-1; print "$head\t$z"; $s =1}elsif($s==1 && $_ ne "N"){$j=$i-1;print "\t$j\n";$s=0}}' chr7.test.fa >> check_Ns
-
+# perl -ne 'chomp;if( />(.*)/){$head = $1; $i=0; next};@a=split("",$_); foreach(@a){$i++; if($_ eq "N" && $s ==0 ){$z=$i-1; print "$head\t$z"; $s =1}elsif($s==1 && $_ ne "N"){$j=$i-1;print "\t$j\n";$s=0}}' chr7.test.fa >> check_Ns
 # SNP count
+# for f in `seq 1 20`; do awk -v var=$f '{OFS="\t";if ($1==var) print }' SNP_count_35kbwin.$f.txt > SNP_count_35kbwin.$f.final.txt; done
 
-for f in `seq 1 20`; do awk -v var=$f '{OFS="\t";if ($1==var) print }' SNP_count_35kbwin.$f.txt > SNP_count_35kbwin.$f.final.txt; done
-
-
-
-
-# Concatenate the output of the 50 jobs per array (20 permutations per job) into a single file for each model
-cat groom_permuted_results_*.txt >> all.groom_permuted_results.txt
-cat prox_permuted_results_*.txt >> all.prox_permuted_results.txt
-
-# Delete individual files for each job in the array as these results are now stored in either the all.groom_permuted_results.txt or all.prox_permuted_results.txt files generated above
-rm groom_permuted_results_*
-rm prox_permuted_results_*
-
-# Check that each dataframe with all of the permutation results has 1,000 lines corresponding to the 1,000 permutations run
-wc -l all.*
-#1000 all.groom_permuted_results.txt
-#1000 all.prox_permuted_results.txt
-#2000 total
 
 # Finally, in R, run 3permuted_pvalue_calculation.R which calculates a permutation based p-value for each predictor variable based on the number of times that the absolute value of the effect size estimated from the permuted data sets was greater than the absolute value of the effect size estimated from the observed data set, across 1,000 permutations. 
