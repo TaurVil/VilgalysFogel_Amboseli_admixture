ad2e1e9991024e3c1ab03c994573d4e42dfcbb5a
ArielleF <afogel29@gmail.com>
2021-07-14 20:36:48 -0400

    Create 2get_ancestry_calls_Wallref.R

---
+++ 02_Local ancestry calling/02.3.pedigree_trios/2get_ancestry_calls_Wallref.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+load("local_ancestry_pedigree_trios_unmaskedWallref.Rd", verbose=T)
+
+# Run R script separately for each chromosome
+tmp <- subset(positions, chrom=="CHROMOSOME") # only positions for focal chromosome
+tmp2 <- subset(tracts2, chrom=="CHROMOSOME") # only tracts from focal chromosome
+
+ancestry_calls <- as.data.frame(matrix(ncol=(nrow(indiv_list)+2), nrow=nrow(tmp)))
+colnames(ancestry_calls)[1:2] <- c("chrom", "pos")
+colnames(ancestry_calls)[3:(nrow(indiv_list)+2)] <- as.character(indiv_list[,1])
+ancestry_calls$chrom <- tmp[,1]
+ancestry_calls$pos <- tmp[,2]
+
+for (k in 1:nrow(indiv_list)) {
+    
+    tmp3 <- tmp2[tmp2$indiv==indiv_list[k,1],]
+    
+  for (j in 1:nrow(tmp)) {
+  
+  tmp4 <- subset(tmp3, tmp3$start <= ancestry_calls$pos[j] & tmp3$end >= ancestry_calls$pos[j])
+  
+      if (nrow(tmp4) == 1) { ancestry_calls[j,k+2] <- tmp4$state }
+      if (nrow(tmp4) > 1) { ancestry_calls[j,k+2] <- "boundary"}
+      if (nrow(tmp4) == 0) { ancestry_calls[j,k+2] <- "not_in_tract"}
+  
+  }
+ print(k)
+     
+}
+
+
+write.table(ancestry_calls,file="ancestry_calls_unmaskedWallref_pedigree_trios_35kbpos_CHROMOSOME.txt",quote=F,row.names=F)
+print("done")
+q(save="no")
