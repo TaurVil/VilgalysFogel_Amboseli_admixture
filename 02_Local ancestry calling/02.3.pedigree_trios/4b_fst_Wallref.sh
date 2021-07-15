51e47b1c68015aeea13967379babb9dfb789bce2
ArielleF <afogel29@gmail.com>
2021-07-14 21:03:19 -0400

    Update 4b_fst_Wallref.sh

--- 02_Local ancestry calling/02.3.pedigree_trios/4b_fst_Wallref.sh
+++ 02_Local ancestry calling/02.3.pedigree_trios/4b_fst_Wallref.sh
@@ -2,7 +2,7 @@
 
 module load vcftools
 
-# should be vcf of all chromosomes, only yellow and anubis reference populations not yet merged with Amboseli
+# VCF containing all chromosomes of masked variants in yellow and anubis reference populations not yet merged with Amboseli
 location=/data/tunglab/tpv/local_ancestry/unadmixed_individuals
 
 vcftools --gzvcf $location/refpanel.vcf.gz --weir-fst-pop 00_anu_wall.list --weir-fst-pop 00_yel_wall.list.nodup --fst-window-size 35000 --fst-window-step 500 --out fst_unmasked_unmerged_Wallref_35kbwin_500bpstep
