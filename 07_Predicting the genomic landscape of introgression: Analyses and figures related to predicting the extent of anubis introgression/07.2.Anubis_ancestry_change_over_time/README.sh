991081dff94895ba1ede54430164c6d53b96afba
ArielleF <afogel29@gmail.com>
2021-07-16 13:17:02 -0400

    Update README.sh

--- 07_Predicting the genomic landscape of introgression: Analyses and figures related to predicting the extent of anubis introgression/07.2.Anubis_ancestry_change_over_time/README.sh
+++ 07_Predicting the genomic landscape of introgression: Analyses and figures related to predicting the extent of anubis introgression/07.2.Anubis_ancestry_change_over_time/README.sh
@@ -1,1 +1,11 @@
+# This directory contains two scripts for evaluating whether genomic features predict the rate of change in anubis allele frequencies over time (Supplementary Methods 14.2).
 
+#############################################################################################################################
+# FST, mean recombination rate, and B values.
+#############################################################################################################################
+
+#############################################################################################################################
+# Get pedigree inconsistences for the pedigree inconsistencies model (scripts labelled 1-3...).
+#############################################################################################################################
+
+# In R, run 1prep_for_pedigree_inconsistencies.R which generates two R data files (local_ancestry_pedigree_trios_maskedSNPRCref.Rd and local_ancestry_pedigree_trios_unmaskedWallref.Rd) containing tracts, pedigree trio info, list of pedigree individuals, and genomic positions for evaluating the consistent of ancestry calls within pedigree trios for two sets of ancestry tracts (one generated using the SNPRC reference panel, one generated using the Wall et al. 2016 Molecular Ecology low coverage reference panel). These R data files can then uploaded to a computing cluster for parallelization across chromosomes (which will make things run much faster).
