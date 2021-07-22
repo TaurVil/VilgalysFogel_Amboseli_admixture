73854c38695c4c3a9ebc78348746ed633023dfc3
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-07-22 17:07:27 -0500

    Update README.sh

--- 06_Selection against regulatory divergence/README.sh
+++ 06_Selection against regulatory divergence/README.sh
@@ -1,1 +1,10 @@
 
+## For background on the expression datasets used in this study, see Tung et al. 2015, Lea et al. 2019, and Anderson et al. 2021
+
+## We began with expression data summarized as read counts per gene and individual, provided here as "expression_data.txt" with accompanying metadata "expression_metadata.txt"
+
+## We process each dataset to normalize and control for covariates. 
+./01.process_expression.R ## run with dataset="eLife" and dataset="TruCulture"
+
+## For each protein coding gene, we calculate the ancestry of each gene, we calculate the ancestry 
+
