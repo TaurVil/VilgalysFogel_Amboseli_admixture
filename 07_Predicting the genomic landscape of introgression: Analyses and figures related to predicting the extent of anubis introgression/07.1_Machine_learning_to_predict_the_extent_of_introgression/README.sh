9678c85cc1776fa51c50947fffe190a989369e99
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-07-22 13:11:41 -0500

    Update README.sh

--- 07_Predicting the genomic landscape of introgression: Analyses and figures related to predicting the extent of anubis introgression/07.1_Machine_learning_to_predict_the_extent_of_introgression/README.sh
+++ 07_Predicting the genomic landscape of introgression: Analyses and figures related to predicting the extent of anubis introgression/07.1_Machine_learning_to_predict_the_extent_of_introgression/README.sh
@@ -1,1 +1,15 @@
 
+## step 1: integrate genomic features matrix
+## we expand upon the genomic features available in Section 5 and VilgalysFogel_main_data_file.RData, to include other possible predictors of anubis ancestry such as gene and enhancer content. 
+
+./01.get_genomic_features.R
+## this script produces 
+
+## step 2: run glmnet
+## in R, load the previous matrix and fit elastic net regression using the R package glmnet (Friedman, Hastie, & Tibshirani, 2010). 
+
+./02.run_glmnet.R
+## this script 
+
+## step 3: plot resulting values
+run figure4C.R
