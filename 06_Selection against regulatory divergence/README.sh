0df48da56a2ea023873c86cd877baa891957d78f
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-07-23 16:21:12 -0500

    typo correction

--- 06_Selection against regulatory divergence/README.sh
+++ 06_Selection against regulatory divergence/README.sh
@@ -6,7 +6,7 @@
 ## For each protein coding gene, we calculate the ancestry of each gene, we calculate the mean anubis ancestry per individual in the population. 
 ./01.assign_gene_ancestry.R
 # Calls a file of chromosome lengths (downloaded from NCBI), the Panubis1 gtf file (downloaded from NCBI), Amboseli ancestry calls (Zenodo), and recombination rates per chromosome (03_Resources)
-# Produces genes_ancestry.RData, which also contains the mean anubis ancestry and the nean recombination rate per gene
+# Produces genes_ancestry.RData, which also contains the mean anubis ancestry and the mean recombination rate per gene
 
 ## We process each dataset to normalize and control for covariates. 
 ./02.process_expression.R ## run with dataset="eLife" and dataset="TruCulture"
