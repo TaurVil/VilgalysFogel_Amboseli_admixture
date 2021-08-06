6ca279cda544001e505a572e97913bbc410b188c
ArielleF <afogel29@gmail.com>
2021-08-06 15:24:00 -0400

    Update 5add_indiv_id.sh

--- 02_Local ancestry calling/02.1.LCLAE/5add_indiv_id.sh
+++ 02_Local ancestry calling/02.1.LCLAE/5add_indiv_id.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for f in `cat 00_vcf_sample_order.masked.list`; do awk 'BEGIN {OFS="\t"} {print $0,FILENAME}' $f.35kb.d2.masked.SNPRCref.txt > tmp.$f.txt; mv tmp.$f.txt $f.35kb.d2.masked.SNPRCref.txt; sed -i 's/.35kb.d2.masked.SWref.txt//g' $f.35kb.d2.masked.SWref.txt; done
+for f in `cat 00_vcf_sample_order.masked.list`; do awk 'BEGIN {OFS="\t"} {print $0,FILENAME}' $f.35kb.d2.masked.SNPRCref.txt > tmp.$f.txt; mv tmp.$f.txt $f.35kb.d2.masked.SNPRCref.txt; sed -i 's/.35kb.d2.masked.SNPRCref.txt//g' $f.35kb.d2.masked.SNPRCref.txt; done
