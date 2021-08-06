897c5145bf3cec00e0d91355b3471ccb9f12a2c6
ArielleF <afogel29@gmail.com>
2021-08-06 15:23:28 -0400

    Update 4concatenate_files_per_indiv.sh

--- 02_Local ancestry calling/02.1.LCLAE/4concatenate_files_per_indiv.sh
+++ 02_Local ancestry calling/02.1.LCLAE/4concatenate_files_per_indiv.sh
@@ -2,4 +2,5 @@
 
 # Concatenate all chromosome files per individual into a single file per individual
 # h is the individual number
-for h in `seq 1 508`; do cat $h.35kb.d2.* > $h.35kb.d2.masked.SWref.txt; done
+# for example, if you want to work on the first 10 individuals in the vcf, run:
+for h in `seq 1 10`; do cat $h.35kb.d2.* > $h.35kb.d2.masked.SNPRCref.txt; done
