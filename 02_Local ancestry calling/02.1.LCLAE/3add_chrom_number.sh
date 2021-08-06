30263017dac6ac1a4c068c0160ca42293c67d457
ArielleF <afogel29@gmail.com>
2021-08-06 15:22:53 -0400

    Update 3add_chrom_number.sh

--- 02_Local ancestry calling/02.1.LCLAE/3add_chrom_number.sh
+++ 02_Local ancestry calling/02.1.LCLAE/3add_chrom_number.sh
@@ -2,4 +2,4 @@
 
 # For chromosomes 1-20 (defined by g)
 # NUMBER defines individual
-for g in `seq 1 20`; do sed -i 's/^/\t/' NUMBER.35kb.d2.$g.*txt; sed -i s/^/$g/g NUMBER.35kb.d2.$g.masked.SWref.txt; done
+for g in `seq 1 20`; do sed -i 's/^/\t/' NUMBER.35kb.d2.$g.*masked.SNPRCref.txt; sed -i s/^/$g/g NUMBER.35kb.d2.$g.masked.SNPRCref.txt; done
