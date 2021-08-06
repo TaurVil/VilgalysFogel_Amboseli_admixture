60a56112bd804a89bba6dfad471ed0db67de6af7
ArielleF <afogel29@gmail.com>
2021-08-06 15:22:30 -0400

    Update 2LCLAE_get_ancestry.sh

--- 02_Local ancestry calling/02.1.LCLAE/2LCLAE_get_ancestry.sh
+++ 02_Local ancestry calling/02.1.LCLAE/2LCLAE_get_ancestry.sh
@@ -8,7 +8,7 @@ printf "`echo NUMBER` `echo $chrom` \n"
 
 number=NUMBER
 
-cat genotype_likelihoods_maskedref/genolik.$chrom.genolik | LCLAE/filtbaboon2c XXX ref_anubis.h  ref_yellow.h $number | LCLAE/geno_lik2 .2 35000 > $number.35kb.d2.$chrom.masked.SWref.txt
+cat genotype_likelihoods_maskedref/genolik.$chrom.genolik | LCLAE/filtbaboon2c XXX ref_anubis.h  ref_yellow.h $number | LCLAE/geno_lik2 .2 35000 > $number.35kb.d2.$chrom.masked.SNPRCref.txt
 
 # LCLAE/filtbaboon2c XXX <-- XXX specifies the total number of individuals in file (e.g., if there are 100 individuals including reference and admixed individuals, put 100 in place of XXX)
 # ref_anubis.h = reference population one (anubis baboons), ref_yellow.h = reference population two (yellow baboons) - LCLAE will estimate the number of alleles from reference population one (here, anubis) so 0 corresponds to 0 anubis alleles, 1 corresponds to 1 anubis allele, and 2 corresponds to 2 anubis alleles)
