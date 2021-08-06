e3c42df0702c7bc714c16ae7f86cb0e9336499bb
ArielleF <afogel29@gmail.com>
2021-08-06 14:56:54 -0400

    Update and rename rerun.2LCLAE_get_ancestry_calls.sh to rerun.2LCLAE_get_ancestry.sh

---
+++ 02_Local ancestry calling/02.1.LCLAE/rerun.2LCLAE_get_ancestry.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+chrom=CHROMOSOME
+
+printf "`echo NUMBER` `echo $chrom` \n"
+
+number=NUMBER
+
+cat ./genotype_likelihoods_maskedref/genolik.$chrom.genolik | LCLAE/filtbaboon2c XXX ref_anubis.h ref_yellow.h $number | LCLAE/geno_lik2 .2 35000 > $number.35kb.d2.$chrom.maskedSWref.txt
+
+# Same as 2LCLAE_get_ancestry.sh script
+# LCLAE/filtbaboon2c XXX <-- XXX specifies the total number of individuals in file (e.g., if there are 100 individuals including reference and admixed individuals, put 100 in place of XXX)
+# ref_anubis.h = reference population one (anubis baboons), ref_yellow.h = reference population two (yellow baboons) - LCLAE will estimate the number of alleles from reference population one (here, anubis) so 0 corresponds to 0 anubis alleles, 1 corresponds to 1 anubis allele, and 2 corresponds to 2 anubis alleles)
+# LCLAE/geno_lik2 0.2 35000 <-- 0.2 specifies the minimum allele frequency difference (here, 20%) between the two reference populations, 35000 specifies the window size (here, 35 kb)
--- 02_Local ancestry calling/02.1.LCLAE/rerun.2LCLAE_get_ancestry_calls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chrom=CHROMOSOME
-
-printf "`echo NUMBER` `echo $chrom` \n"
-
-number=NUMBER
-
-cat ./genotype_likelihoods_maskedref/genolik.$chrom.genolik | LCLAE/filtbaboon2c 508 ref_anubis.h ref_yellow.h $number | LCLAE/geno_lik2 .2 35000 > $number.35kb.d2.$chrom.maskedSWref.txt
