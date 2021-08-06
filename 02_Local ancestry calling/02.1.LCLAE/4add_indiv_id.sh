4fc82fda6e7562d8a50e3102e0abf785a4b8c630
ArielleF <afogel29@gmail.com>
2021-08-06 15:10:46 -0400

    Create 4add_indiv_id.sh

---
+++ 02_Local ancestry calling/02.1.LCLAE/4add_indiv_id.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for f in `cat 00_vcf_sample_order.masked.list`; do awk 'BEGIN {OFS="\t"} {print $0,FILENAME}' $f.35kb.d2.masked.SWref.txt > tmp.$f.txt; mv tmp.$f.txt $f.35kb.d2.masked.SWref.txt; sed -i 's/.35kb.d2.masked.SWref.txt//g' $f.35kb.d2.masked.SWref.txt; done
