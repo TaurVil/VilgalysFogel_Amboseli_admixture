a3b7e35ed0a73153c228501630747476f583cc63
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-08-06 11:37:16 -0500

    Update README.sh

--- 01_Genotype_Calling/README.sh
+++ 01_Genotype_Calling/README.sh
@@ -1,2 +1,4 @@
 
 Scripts to process whole genome short-read Illumina sequence data (fastq -> BAM -> gVCF).
+
+For any questions regarding this content, please contact Jacqueline Robinson (https://jarobinsonresearch.com). 
