ade6efa2629207069ba385193dc0e23fb16a9f30
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-08-08 11:59:53 -0500

    Update and rename README.sh to README.md

--- 01_Genotype_Calling/README.sh
+++
@@ -1,4 +0,0 @@
-
-Scripts to process whole genome short-read Illumina sequence data (fastq -> BAM -> gVCF).
-
-For any questions regarding this content, please contact Jacqueline Robinson (https://jarobinsonresearch.com). 
