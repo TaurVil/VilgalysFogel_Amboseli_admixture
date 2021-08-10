2375faa3aeb53e7e11ebbccc6098e7695a93c5be
ArielleF <afogel29@gmail.com>
2021-08-10 16:58:34 -0400

    Update 06a.get_eigenstrat_format_excluding_chr02s.sh

--- 02_Local ancestry calling/02.4.F4_statistics/06a.get_eigenstrat_format_excluding_chr02s.sh
+++ 02_Local ancestry calling/02.4.F4_statistics/06a.get_eigenstrat_format_excluding_chr02s.sh
@@ -20,7 +20,7 @@ module load plink
 plink --file CHROM.n49 --update-ids updatepop.n49.txt --keep-allele-order --recode --out updated.n49.CHROM
 
 # Also, need to convert our column 6 from weird 0 values to 1 (otherwise individuals are ignored)
-plink --file updated.n49.CHROM --make-pheno names.pheno.txt '*' --allow-no-sex --keep-allele-order --recode --out updated2.n49.CHROM # names.pheno.txt is just the first two columns of updatepop.n48.txt
+plink --file updated.n49.CHROM --make-pheno names.pheno.txt '*' --allow-no-sex --keep-allele-order --recode --out updated2.n49.CHROM # names.pheno.txt is just the first two columns of updatepop.n49.txt
 
 # Finally, convert plink format to eigenstrat format using the convertf parameter
 EIG-6.1.4/bin/convertf -p my.par.ped.eigenstrat.CHROM
