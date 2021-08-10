fcb3372a12a11be42a989c17c69512ecad22ed92
ArielleF <afogel29@gmail.com>
2021-08-10 12:47:24 -0400

    Update and rename 01b.bowtie2_map_macam.SRR_data.sh to 1b.bowtie2_map_macam.SRR_data.sh

--- 02_Local ancestry calling/02.4.F4_statistics/1b.bowtie2_map_macam.SRR_data.sh
+++ 02_Local ancestry calling/02.4.F4_statistics/1b.bowtie2_map_macam.SRR_data.sh
@@ -3,6 +3,7 @@
 module load bowtie2
 module load samtools
 
+# for fastq files where paired-end data are labeled as _1 and _2
 bowtie2 -p 16 -t -x MacaM_bowtie2/MacaM -1 NAME*_1*.fastq.gz -2 NAME*_2*.fastq.gz -S mapped.NAME.sam --no-unal
 
 samtools view -bS mapped.NAME.sam > mapped.NAME.bam
