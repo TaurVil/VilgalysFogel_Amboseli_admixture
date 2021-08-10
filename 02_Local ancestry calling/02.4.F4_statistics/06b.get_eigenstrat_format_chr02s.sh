cb0f67bc896f04faff518bfaa054f84b073908b5
ArielleF <afogel29@gmail.com>
2021-08-10 16:36:56 -0400

    Create  06b.get_eigenstrat_format_chr02s.sh

---
+++ 02_Local ancestry calling/02.4.F4_statistics/06b.get_eigenstrat_format_chr02s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Need to convert vcfs into eigenstrat format in order to use ADMIXTOOLS/admixr
+# To do so, convert vcf --> plink format --> eigenstrat format
+
+module load vcftools
+vcftools --gzvcf final.baboon.to.macam.n49.CHROM.rename.vcf.gz --mac 1 --plink --out CHROM.n49
+
+# Second, update plink ped file with correct population info for each sample
+#--update-ids expects input with the following four fields:
+
+#Old family ID
+#Old within-family ID
+#New family ID
+#New within-family ID
+
+module load plink
+
+plink --file CHROM.n49 --update-ids updatepop.n49.txt --keep-allele-order --recode --out updated.n49.CHROM
+
+# Also, need to convert our column 6 from weird 0 values to 1 (otherwise individuals are ignored)
+plink --file updated.n49.CHROM --make-pheno names.pheno.txt '*' --allow-no-sex --keep-allele-order --recode --out updated2.n49.CHROM # names.pheno.txt is just the first two columns of updatepop.n48.txt
+
+# Finally, convert plink format to eigenstrat format using the convertf parameter
+EIG-6.1.4/bin/convertf -p my.par.ped.eigenstrat.CHROM
