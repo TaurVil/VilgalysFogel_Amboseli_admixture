aee53001cd4f92c78e1e518d7965b50d9a4a5ca3
ArielleF <afogel29@gmail.com>
2021-08-14 15:56:51 -0400

    Update run.05.add_indiv_id.sh

--- 02_local-ancestry-calling/02.1.LCLAE/run.05.add_indiv_id.sh
+++ 02_local-ancestry-calling/02.1.LCLAE/run.05.add_indiv_id.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Add a column with the individual's id
 for f in `cat 00_vcf_sample_order.masked.list`; do awk 'BEGIN {OFS="\t"} {print $0,FILENAME}' $f.35kb.d2.masked.SNPRCref.txt > tmp.$f.txt; mv tmp.$f.txt $f.35kb.d2.masked.SNPRCref.txt; sed -i 's/.35kb.d2.masked.SNPRCref.txt//g' $f.35kb.d2.masked.SNPRCref.txt; done
