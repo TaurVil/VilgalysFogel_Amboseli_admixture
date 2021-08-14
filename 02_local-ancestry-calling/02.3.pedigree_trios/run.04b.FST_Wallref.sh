8a6385775ff69b68d9aca185508ff0b10201bea6
ArielleF <afogel29@gmail.com>
2021-08-14 18:29:02 -0400

    Delete run.04b.FST_Wallref.sh

--- 02_local-ancestry-calling/02.3.pedigree_trios/run.04b.FST_Wallref.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load vcftools
-
-# VCF containing all chromosomes of masked variants in yellow and anubis reference populations not yet merged with Amboseli individuals
-# this VCF file is available from Zenodo
-
-vcftools --gzvcf masked_yellow_and_anubis.vcf.gz --weir-fst-pop Wallanubis.list --weir-fst-pop Wallyellow.list --fst-window-size 35000 --fst-window-step 500 --out fst_unmasked_unmerged_Wallref_35kbwin_500bpstep
