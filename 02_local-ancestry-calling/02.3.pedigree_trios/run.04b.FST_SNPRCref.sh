a522d93d18c3a426380635f049ea8574c8d60b4a
ArielleF <afogel29@gmail.com>
2021-08-16 12:05:34 -0400

    Update run.04b.FST_SNPRCref.sh

--- 02_local-ancestry-calling/02.3.pedigree_trios/run.04b.FST_SNPRCref.sh
+++ 02_local-ancestry-calling/02.3.pedigree_trios/run.04b.FST_SNPRCref.sh
@@ -5,4 +5,4 @@ module load vcftools
 # VCF containing all chromosomes of masked variants in yellow and anubis reference populations not yet merged with Amboseli individuals
 # this VCF file is available from Zenodo
 
-vcftools --gzvcf masked_yellow_and_anubis.vcf.gz  --weir-fst-pop SNPRCanubis.list --weir-fst-pop SNPRCyellow.list --fst-window-size 35000 --fst-window-step 500 --out fst_masked_unmerged_SWref_35kbwin_500bpstep
+vcftools --gzvcf masked_yellow_and_anubis.vcf.gz  --weir-fst-pop SNPRCanubis.list --weir-fst-pop SNPRCyellow.list --fst-window-size 35000 --fst-window-step 500 --out fst_masked_unmerged_SNPRCref_35kbwin_500bpstep
