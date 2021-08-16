764544f2bd5f3cafa321b581e3f5c508e26e4023
ArielleF <afogel29@gmail.com>
2021-08-16 18:00:05 -0400

    Update run.06a.get_eigenstrat_format_excluding_chr02s.sh

--- 02_local-ancestry-calling/02.4.F4_statistics/run.06a.get_eigenstrat_format_excluding_chr02s.sh
+++ 02_local-ancestry-calling/02.4.F4_statistics/run.06a.get_eigenstrat_format_excluding_chr02s.sh
@@ -6,7 +6,7 @@
 # First, convert vcf to plink format
 module load vcftools
 vcftools --gzvcf final.baboon.to.macam.n49.CHROM.vcf.gz --mac 1 --plink --out CHROM.n49
-# mac 1 = only include sites with a minor allele count of at least 1 (i.e., all samples are not all carrying the major allele) - doesn’t really matter because sites where individuals are completely for the major allele won’t be informative for f-statistics
+# mac 1 = only include sites with a minor allele count of at least 1 (i.e., all samples are not all carrying the major allele) - this filtering is not really necessary because sites where all samples only have the major allele won’t be informative for f-statistics
 
 # Second, update plink ped file with correct population info for each sample
 #--update-ids expects input with the following four fields:
@@ -23,5 +23,5 @@ plink --file CHROM.n49 --update-ids updatepop.n49.txt --keep-allele-order --reco
 # Also, need to convert our column 6 from weird 0 values to 1 (otherwise individuals are ignored)
 plink --file updated.n49.CHROM --make-pheno names.pheno.txt '*' --allow-no-sex --keep-allele-order --recode --out updated2.n49.CHROM # names.pheno.txt is just the first two columns of updatepop.n49.txt
 
-# Finally, convert plink format to eigenstrat format using convertf
+# Finally, convert plink format to eigenstrat format using convertf program from EIGENSOFT
 EIG-6.1.4/bin/convertf -p my.par.ped.eigenstrat.CHROM
