2c28a42cf9aa798dde84800a40c754d8e0d71406
ArielleF <afogel29@gmail.com>
2021-08-17 15:57:38 -0400

    Update and rename r01.anubis_ancestry_change_over_time.R to run.01.anubis_ancestry_change_over_time.R

--- 07_landscape-of-introgression/07.2.anubis_ancestry_over_time/run.01.anubis_ancestry_change_over_time.R
+++ 07_landscape-of-introgression/07.2.anubis_ancestry_over_time/run.01.anubis_ancestry_change_over_time.R
@@ -37,9 +37,9 @@ colnames(years)[2] <- c("year")
 # How many individuals are present in the population every year?
 tmp <- years %>% group_by(year) %>% tally()
 # Visualize the number of individuals present in the population every year
-ggplot(data=tmp) +geom_col(aes(year, n), color="white") + geom_text(aes(year,n, label=n), vjust=-1, size=3) + theme_classic() + theme(text=element_text(size=12), axis.text = element_text(color="black")) #+ geom_vline(aes(xintercept=1979-0.5), linetype="dashed", color="steelblue3", size=1) # we have between 1-5 individuals through 1978 and then approximately double individuals in our sample in 1979 to 11 
+ggplot(data=tmp) +geom_col(aes(year, n), color="white") + geom_text(aes(year,n, label=n), vjust=-1, size=3) + theme_classic() + theme(text=element_text(size=12), axis.text = element_text(color="black")) #+ geom_vline(aes(xintercept=1979-0.5), linetype="dashed", color="steelblue3", size=1) # we have between 1-5 individuals through 1978 and then the sample size approximately doubles in 1979 to 11 
 
-# We'll start our analysis in 1979 where we begin to have more than just a handful of individuals so that we can get an estimate of average ancestry in the population per year
+# We'll start our analysis in 1979 where we begin to have more than just a handful of individuals so that we can get an estimate of average anubis ancestry in the population per year
 tmp <- tmp[tmp$year>=1979,]
 min(tmp$n) # minimum of 11 individuals resequenced per year
 max(tmp$n) # maximum of 228 individuals resequenced per year
@@ -56,14 +56,14 @@ for (i in 1:length(years_list)) {
   
   tmp2 <- ancestry_windows[c(1:3,k)]
   tmp2$na_count <- rowSums(is.na(tmp2[c(4:ncol(tmp2))])) # count any NAs 
-  tmp2$pop_avg_anubis_admix <- (rowSums(tmp2[c(4:(ncol(tmp2)-1))], na.rm=T))/(length(k)-tmp2$na_count) # get the average anubis ancestry at each 250 kb genomic window (accounting for any individuals wiht NAs)
-  tmp2$indiv_count <- length(k) # add the number of individuals included in the focal year year in the population (the number of individuals actually used for estimating average anubis ancestry at each site would be indiv_count-na_count)
+  tmp2$pop_avg_anubis_admix <- (rowSums(tmp2[c(4:(ncol(tmp2)-1))], na.rm=T))/(length(k)-tmp2$na_count) # get the average anubis ancestry for each 100 kb genomic window (accounting for any individuals with NAs)
+  tmp2$indiv_count <- length(k) # add the number of individuals included in the focal year in the population (the number of individuals actually used for estimating average anubis ancestry at each site would be indiv_count-na_count)
   
   tmp3 <- tmp2[names(tmp2) %in% c("chr", "start", "end", "na_count", "pop_avg_anubis_admix", "indiv_count")] # only keep the columns we need
   tmp3[,7] <- years_list[i] # add the focal year
   
   if (i==1) {tmp3 -> ancestry_year} # if this is the first year, create a new data frame called ancestry_year
-  if (i>1) {rbind(ancestry_year, tmp3) -> ancestry_year} # if this is not the first year, add to the focal year's data to the existing data frame ancestry_year
+  if (i>1) {rbind(ancestry_year, tmp3) -> ancestry_year} # if this is not the first year, add the focal year's data to the existing data frame ancestry_year
   
   
   print(i)
@@ -77,7 +77,7 @@ ancestry_year$pos <- ((ancestry_year$end-ancestry_year$start)/2)+ancestry_year$s
 # Make a unique id per window
 ancestry_year$id <- paste(ancestry_year$chr, ancestry_year$pos, sep="_")
 
-# For each genomic window, calculate the change in anubis ancestry over time (get the slope) 
+# For each genomic window, calculate the change in anubis ancestry over time (get the beta for the year effect in a linear model of mean anubis ancestry per year ~ year) 
 # Get a list of the unique genomic windows we'll be evaluating
 ancestry2 <- ancestry_windows %>% select(chr, start, end)
 
@@ -88,14 +88,13 @@ ancestry2$id <- paste(ancestry2$chr, ancestry2$pos, sep="_")
 
 ancestry_change <- ancestry2 
 
-## note: this would be consiberably faster as a function and done in parallel
-for (i in 1:nrow(ancestry2)) {
+for (i in 1:nrow(ancestry2)) { # for each genomic window
   
   tmp <- ancestry_year[ancestry_year$id==ancestry2$id[i],]
   ancestry_change[i,6] <- lm(pop_avg_anubis_admix~year, data=tmp)$coeff[2] # grab the beta for year
   tmp2 <- tmp[tmp$year==min(tmp$year),]
   ancestry_change[i,7] <- tmp2$pop_avg_anubis_admix # grab the average anubis ancestry in the population in the starting year of the analysis (1979)
-  ancestry_change[i,8] <- nrow(tmp) #  number of years included in the model (should be 42 years for every genomic window since we have no NAs for average anubis ancestry in the population in the ancestry_year data frame)
+  ancestry_change[i,8] <- nrow(tmp) # number of years included in the model (should be 42 years for every genomic window since we have no NAs for average anubis ancestry in the population in the ancestry_year data frame)
   
   if (i/500 == round(i/500)) {print(i)}
 }
@@ -105,7 +104,7 @@ colnames(ancestry_change)[6:8] <- c("annual_anubis_change_beta", "starting_pop_a
 min(ancestry_change$year_count) # all genomic windows used 42 years (what we expect)
 
 # Add genomic features to the ancestry_change data frame so we can evaluate whether the change in anubis ancestry over time is not only predicted by the starting anubis frequency in 1979, but also FST, mean recombination rate, and B values
-# 1. FST - to get these estimates, use the script 4bFST_SNPRCref.sh in "VilgalysFogel_Amboseli_admixture/02_Local ancestry calling/02.3.pedigree_trios/" but substituting 100000 for window size --fst-window-size in place of 35000.
+# 1. FST - to get these estimates, use the script 4bFST_SNPRCref.sh in "VilgalysFogel_Amboseli_admixture/02_local-ancestry-calling/02.3.pedigree_trios/" but substituting 100000 for window size --fst-window-size in place of 35000.
 fst <- read.table("fst_masked_unmerged_SWref_100kbwin_500bpstep.windowed.weir.fst", header=T) 
 # Remove mean FST as we will use weighted Fst (although results are qualitatively similar whether you use one measure or the other)
 fst <- fst[c(1:5)] 
@@ -119,7 +118,7 @@ fst <- fst[c(4,5,7)]
 # Merge FST estimates with ancestry_change data frame
 all_data <- merge(ancestry_change, fst, by=c("id"), all.x=T)
 summary(all_data) # we have three genomic windows with no FST estimates
-subset(all_data, is.na(N_VARIANTS)) # looks like it is because there are no variants - but how do we have ancestry in those three windows then? if you check the ancestry tracts file("amboseli.tracts.maskedSNPRCref.txt") available in the Duke Data Repository at, many individuals have a single tract that spans the entire window. Otherwise, looks like they have two tracts flanking the start and end.
+subset(all_data, is.na(N_VARIANTS)) # looks like it is because there are no variants - but how do we have ancestry in those three windows then? if you check the ancestry tracts file ("amboseli_LCLAE_tracts.txt"), many individuals have a single tract that spans the entire window. Otherwise, looks like they have two tracts flanking the start and end.
 # chr2_84375000 chr2 84250000 84500000 84375000 
 # nrow(subset(tracts, chrom=="chr2" & start<84250000 & end>84500000)) # 323 individuals have a single tract that spans the entire window
 # chr2_84625000 chr2 84500000 84750000 84625000
