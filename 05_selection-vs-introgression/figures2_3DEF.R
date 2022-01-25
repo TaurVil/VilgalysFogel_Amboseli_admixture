4e6a7195cfe6e3fd1c44cb228012f07fcf17ab77
ArielleF <afogel29@gmail.com>
2022-01-25 16:09:47 -0500

    Update figures2_3DEF.R

--- 05_selection-vs-introgression/figures2_3DEF.R
+++ 05_selection-vs-introgression/figures2_3DEF.R
@@ -36,7 +36,7 @@ plotA <- ggplot(data = baboon, aes(x=fixed_quant, y=mean_ancestry)) +
   geom_boxplot(width=0.15,outlier.shape = NA, size=1) +
   geom_hline(aes(yintercept=median_anubis_ancestry), linetype="dashed", size=0.75, color="grey50") +
   theme_classic() + 
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10))  + 
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10))  + 
   labs(x="fixed differences quantile", y = "") + scale_color_gradient(low="#B09FD1", high="#5C438A"); plotA
 
 # Plot B values vs. introgressed anubis ancestry
@@ -45,7 +45,7 @@ plotC <- ggplot(data = baboon, aes(x=B_quant, y=mean_ancestry)) +
   geom_boxplot(width=0.15,outlier.shape = NA, size=1) + 
   geom_hline(aes(yintercept=median_anubis_ancestry), linetype="dashed", size=0.75, color="grey50") +
   theme_classic() + 
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10))  +
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10))  +
   labs(x="B value quantile", y = "proportion of introgressed (anubis) ancestry") + scale_color_gradient(low="#9CAED3", high="#506FB3"); plotC
 
 # Plot recombination rate vs. introgressed anubis ancestry
@@ -54,7 +54,7 @@ plotE <- ggplot(data = baboon, aes(x=rcr_quant, y=mean_ancestry)) +
   geom_boxplot(width=0.15,outlier.shape = NA, size=1) +
   geom_hline(aes(yintercept=median_anubis_ancestry), linetype="dashed", size=0.75, color="grey50") +
   theme_classic() + 
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10)) + 
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), legend.position = "none", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_text(vjust=-2), plot.margin = margin(b=10)) + 
   labs(x="recombination rate quantile", y = "") + scale_color_gradient(low="#9BC6D4", high="#366B7D"); plotE
 
 
@@ -94,19 +94,19 @@ hominin$mean_ancestry_centered_sd <- hominin$mean_ancestry_centered/ sd(hominin$
 # Plot fixed differences vs. introgressed ancestry for hominins and baboons
 plotB <- ggplot() + 
   geom_smooth(data = hominin, aes(x=rank(fixed_N3)/length(fixed_N3), y=mean_ancestry_centered_sd), color="#5C438A", size=1.5, linetype = "longdash", method="lm", se=FALSE) + geom_smooth(data = baboon, aes(x=rank(fixed)/length(fixed), y=mean_ancestry_centered_sd), color="#5C438A", size=1.5, method="lm", se=FALSE) + theme_classic() +
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20, b=10))  + 
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20, b=10))  + 
   labs(x="rank-ordered number of fixed differences", y=""); plotB
 
 # Plot B values vs. introgressed ancestry for hominins and baboons
 plotD <- ggplot() + 
   geom_smooth(data = hominin, aes(x=rank(B)/length(B), y=mean_ancestry_centered_sd), color="#506FB3", size=1.5, linetype = "longdash", method="lm", se=FALSE) + geom_smooth(data = baboon, aes(x=rank(B)/length(B), y=mean_ancestry_centered_sd), color="#506FB3", size=1.5, method="lm", se=FALSE) + theme_classic() +
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20, b=10))  + 
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20, b=10))  + 
   labs(x="rank-ordered B values", y="standardized introgressed ancestry"); plotD
 
 # Plot recombination rate vs. introgressed ancestry for hominins and baboons
 plotF <- ggplot() + 
   geom_smooth(data = hominin, aes(x=rank(recombination)/length(recombination), y=mean_ancestry_centered_sd), color="#366B7D", size=1.5, linetype = "longdash", method="lm", se=FALSE) + geom_smooth(data = baboon, aes(x=rank(rcr)/length(rcr), y=mean_ancestry_centered_sd), color="#366B7D", size=1.5,  method="lm", se=FALSE) + theme_classic() +
-  theme(text=element_text(size=14), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20,b=10))  + 
+  theme(text=element_text(size=14, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_blank(),axis.ticks.x=element_blank(), legend.position = "none", axis.title.x = element_text(vjust=-2), plot.margin = margin(l=20,b=10))  + 
   labs(x="rank-ordered recombination rate", y=""); plotF
 
 # Plot all panels in figure 2
@@ -132,19 +132,19 @@ baboon$historical_ancestry_centered <- baboon$historical_ancestry - mean(baboon$
 # Plot fixed differences vs. introgressed recent and historical anubis ancestry
 baboon$fixed[baboon$fixed > 30] <- 30 # classify all fixed differences greater than 30 in a "30+" bin which we'll note on the x-axis
 
-plotDleft <- ggplot(data = baboon) +  geom_jitter(aes(x=fixed, y=recent_ancestry), alpha = 0.08, col="#5B507A") + geom_smooth(aes(x=fixed, y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_jitter(aes(x=fixed, y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=fixed, y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black")) + scale_x_continuous(labels=c("0","10","20","30+")) + xlab(label="") + scale_y_continuous(name= "proportion of anubis ancestry"); plotDleft
+plotDleft <- ggplot(data = baboon) +  geom_jitter(aes(x=fixed, y=recent_ancestry), alpha = 0.08, col="#5B507A") + geom_smooth(aes(x=fixed, y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_jitter(aes(x=fixed, y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=fixed, y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black")) + scale_x_continuous(labels=c("0","10","20","30+")) + xlab(label="") + scale_y_continuous(name= "proportion of anubis ancestry"); plotDleft
 
-plotDright <- ggplot(data = baboon) + geom_smooth(aes(x=fixed, y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=fixed, y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black"))  +  coord_cartesian(ylim=c(-0.04, 0.04)) + scale_x_continuous(labels=c("0","10","20","30+")) + xlab(label="") + scale_y_continuous(name = "proportion of anubis ancestry\n- proportion of mean anubis ancestry"); plotDright 
+plotDright <- ggplot(data = baboon) + geom_smooth(aes(x=fixed, y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=fixed, y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black"))  +  coord_cartesian(ylim=c(-0.04, 0.04)) + scale_x_continuous(labels=c("0","10","20","30+")) + xlab(label="") + scale_y_continuous(name = "proportion of anubis ancestry\n- proportion of mean anubis ancestry"); plotDright 
 
 # Plot ranked ordered B values vs. introgressed recent and historical anubis ancestry
-plotEleft <- ggplot(data = baboon) +  geom_point(aes(x=rank(B), y=recent_ancestry), alpha = 0.08, col="#5B507A") +geom_smooth(aes(x=rank(B), y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_point(aes(x=rank(B), y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=rank(B), y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"), axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry"); plotEleft
+plotEleft <- ggplot(data = baboon) +  geom_point(aes(x=rank(B), y=recent_ancestry), alpha = 0.08, col="#5B507A") +geom_smooth(aes(x=rank(B), y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_point(aes(x=rank(B), y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=rank(B), y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"), axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry"); plotEleft
 
-plotEright <- ggplot(data = baboon) + geom_smooth(aes(x=rank(B), y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=rank(B), y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"),axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry\n- proportion of mean anubis ancestry") +  coord_cartesian(ylim=c(-0.04, 0.04)); plotEright
+plotEright <- ggplot(data = baboon) + geom_smooth(aes(x=rank(B), y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=rank(B), y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"),axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry\n- proportion of mean anubis ancestry") +  coord_cartesian(ylim=c(-0.04, 0.04)); plotEright
 
 # Plot ranked ordered recombination rate vs. introgressed recent and historical anubis ancestry
-plotFleft <- ggplot(data = baboon) +  geom_point(aes(x=rank(rcr), y=recent_ancestry), alpha = 0.08, col="#5B507A") + geom_smooth(aes(x=rank(rcr), y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_point(aes(x=rank(rcr), y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=rank(rcr), y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"),axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry"); plotFleft
+plotFleft <- ggplot(data = baboon) +  geom_point(aes(x=rank(rcr), y=recent_ancestry), alpha = 0.08, col="#5B507A") + geom_smooth(aes(x=rank(rcr), y=recent_ancestry), method="lm", color="#5B507A", size=1.5, linetype = "longdash") + geom_point(aes(x=rank(rcr), y=historical_ancestry), alpha = 0.08, col="#AD7748") + geom_smooth(aes(x=rank(rcr), y=historical_ancestry), method="lm", color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"),axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry"); plotFleft
 
-plotFright <- ggplot(data = baboon)  + geom_smooth(aes(x=rank(rcr), y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=rank(rcr), y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"), axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry\n- proportion of mean anubis ancestry") + coord_cartesian(ylim=c(-0.04, 0.04)); plotFright
+plotFright <- ggplot(data = baboon)  + geom_smooth(aes(x=rank(rcr), y=recent_ancestry_centered), method="lm", se=FALSE, color="#5B507A", size=1.5, linetype = "longdash") + geom_smooth(aes(x=rank(rcr), y=historical_ancestry_centered), method="lm", se=FALSE, color="#AD7748", size=1.5, linetype = "longdash") + theme_classic() + theme(text=element_text(size=25, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x=element_text(color="white"), axis.ticks.x=element_blank()) + xlab(label="") + ylab(label = "proportion of anubis ancestry\n- proportion of mean anubis ancestry") + coord_cartesian(ylim=c(-0.04, 0.04)); plotFright
 
 # Plot all panels in figures 3D-F
 (((plotDleft + theme(plot.margin = unit(c(0,0,50,0), "pt"))) | plotDright) + xlab(label = "number of fixed differences per Mb") + theme(axis.title.x = element_text( hjust=2.25, vjust=-0.3))) / (((plotEleft + theme(plot.margin = unit(c(0,0,50,0), "pt"))) | plotEright) + xlab(label = "rank-ordered B values") + theme(axis.title.x = element_text(hjust=-6,vjust=-0.3))) / (((plotFleft + theme(plot.margin = unit(c(0,0,50,0), "pt"))) | plotFright) + xlab(label = "rank-ordered recombination rate") + theme(axis.title.x = element_text(hjust=3, vjust=-0.3)))
