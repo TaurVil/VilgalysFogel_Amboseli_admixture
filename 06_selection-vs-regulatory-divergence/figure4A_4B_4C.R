8f7e7a1cb215f96b7af8b74499d805bd559e16da
ArielleF <afogel29@gmail.com>
2022-01-26 12:53:28 -0500

    Update figure4A_4B_4C.R

--- 06_selection-vs-regulatory-divergence/figure4A_4B_4C.R
+++ 06_selection-vs-regulatory-divergence/figure4A_4B_4C.R
@@ -18,7 +18,7 @@ ggplot(gene) +
   geom_boxplot(aes(x=as.factor(nearest_state), y=expression), width=0.1, size=0.75) + 
   scale_fill_manual(values=c("#FFED4F", "orange2", "#009E73")) + 
   scale_x_discrete(name="", labels=c("0" = "homozygous\nyellow", "1" = "heterozygous", "2" = "homozygous\nanubis")) + scale_y_continuous(name="normalized, residual\ngene expression") +
-  theme_classic() + theme(legend.position = "none", axis.text = element_text(colour = "black"), text=element_text(size=20)) -> a
+  theme_classic() + theme(legend.position = "none", axis.text = element_text(colour = "black"), text=element_text(size=20, family="Helvetica")) -> a
 
 ggsave("fig4A_main.png")
 
@@ -37,7 +37,7 @@ ggplot(data=reduction, aes(x=as.factor(qqq), y=t_data, group=qqq)) +
   scale_x_discrete(name="proportion of genes", 
                    labels=c("0.1" = "top 10%\nvs.\nbottom 10%", "0.15" = "15%", "0.2" = "top 20%\nvs.\nbottom 20%", "0.25" = "25%","0.3" = "top 30%\nvs.\nbottom 30%", "0.35" = "35%","0.4" = "top 40%\nvs.\nbottom 40%")) + 
   scale_y_continuous(name="\u0394 anubis ancestry\n(least DE - most DE)", limits=c(-0.13, 0.13))  +  
-  theme_classic() +  theme(text=element_text(size=20), axis.text = element_text(color="black"), axis.text.x = element_text(face="italic", size=12), axis.title.x=element_text(vjust=-1))  -> b 
+  theme_classic() +  theme(text=element_text(size=20, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x = element_text(face="italic", size=12, family="Helvetica"), axis.title.x=element_text(vjust=-1))  -> b 
 
 ggsave("fig4B.png")
 
@@ -56,6 +56,6 @@ subset(results, bootstrap_pval<0.05)$quantile
 ggplot(data=results) +
   geom_pointrange(aes(x=quantile, y=rho_DE, ymin=rho_DE - sd_DE, ymax=rho_DE + sd_DE), fill="#0192CB", color="#0192CB", size=1, shape=21, stroke=1.5) +
   geom_pointrange(aes(x=quantile+0.01, y=rho_nDE, ymin=rho_nDE -  sd_nDE, ymax=rho_nDE + sd_nDE), fill="#965F8E", color="#965F8E", size=1, shape=21, stroke=1.5) +
-  scale_x_continuous(name="proportion of genes", labels=c("0.1" = "top 10%\nvs.\nbottom 10%", "0.15" = "15%", "0.2" = "top 20%\nvs.\nbottom 20%", "0.25" = "25%","0.3" = "top 30%\nvs.\nbottom 30%", "0.35" = "35%","0.4" = "top 40%\nvs.\nbottom 40%"), breaks=seq(0.1,0.4, 0.05)) + scale_y_continuous("Spearman's rho", limits=c(0, 0.2)) +  theme_classic() +  theme(text=element_text(size=20), axis.text = element_text(color="black"), axis.text.x = element_text(face="italic", size=12), axis.title.x=element_text(vjust=-1)) + annotate("text", x = 0.15, y = 0.18, label = "*", size=8) +  annotate("text", x = 0.2, y = 0.18, label = "*", size=8) -> c
+  scale_x_continuous(name="proportion of genes", labels=c("0.1" = "top 10%\nvs.\nbottom 10%", "0.15" = "15%", "0.2" = "top 20%\nvs.\nbottom 20%", "0.25" = "25%","0.3" = "top 30%\nvs.\nbottom 30%", "0.35" = "35%","0.4" = "top 40%\nvs.\nbottom 40%"), breaks=seq(0.1,0.4, 0.05)) + scale_y_continuous("Spearman's rho", limits=c(0, 0.2)) +  theme_classic() +  theme(text=element_text(size=20, family="Helvetica"), axis.text = element_text(color="black"), axis.text.x = element_text(face="italic", size=12, family="Helvetica"), axis.title.x=element_text(vjust=-1)) + annotate("text", x = 0.15, y = 0.18, label = "*", size=8) +  annotate("text", x = 0.2, y = 0.18, label = "*", size=8) -> c
 
 ggsave("fig4C.png")
